b9bdc57c10c652d116f5348dd1c5a5cfaa152aeb
Miguel Chan <8616175+MiguelChan@users.noreply.github.com>
2021-10-30 17:17:15 -0700

    [Issues-4] Initial Service and Repository Setup (#5)

---
+++ authorization-service/src/main/java/com/mchan/authorization/service/AuthorizationServiceApplication.java
@@ -0,0 +1,16 @@
+package com.mchan.authorization.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main AuthorizationService Application.
+ */
+@SpringBootApplication
+public class AuthorizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServiceApplication.class, args);
+    }
+
+}
---
+++ authorization-service/src/test/java/com/mchan/authorization/service/AuthorizationServiceApplicationTests.java
@@ -0,0 +1,26 @@
+package com.mchan.authorization.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests.
+ */
+public class AuthorizationServiceApplicationTests {
+
+    /**
+     * BeforeEach method.
+     */
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void someMethod_should_doSomething() {
+        assertThat(true).isTrue();
+    }
+
+}
